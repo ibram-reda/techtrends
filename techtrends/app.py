1179e042be4c00c39f51419aa7fc7e48116174fb
Ibram Reda <abramreda815@gmail.com>
2022-12-09 18:30:15 +0200

    implement step 1, Best Practics For Application Deployment

--- techtrends/app.py
+++ techtrends/app.py
@@ -1,13 +1,19 @@
 import sqlite3
-
+import logging
+from datetime import datetime
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+
+# variable to keep track of total connection to database
+db_connection_count = 0
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    global db_connection_count
+    db_connection_count += 1
     return connection
 
 # Function to get a post using its ID
@@ -17,6 +23,13 @@ def get_post(post_id):
                         (post_id,)).fetchone()
     connection.close()
     return post
+
+# Function to get a total post Count in the db
+def get_post_count():
+    connection = get_db_connection()
+    post_count = connection.execute('SELECT COUNT(id) as count FROM posts').fetchone()  
+    connection.close()
+    return post_count['count'] 
 
 # Define the Flask application
 app = Flask(__name__)
@@ -36,13 +49,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.info(f"{datetime.now()}, Article with id = {post_id} Not Found!")
       return render_template('404.html'), 404
     else:
+      app.logger.info(f"{datetime.now()}, Article '{post['title']}' retrieved!")
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info(f"{datetime.now()}, about page has been reached")
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -60,11 +76,32 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
+            app.logger.info(f"{datetime.now()}, Artical '{title}' Created")
 
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
+# HealthCheck endPoint
+@app.route('/healthz')
+def status():
+  response = app.response_class(
+          response=json.dumps({"result":"OK - healthy"}),
+          status=200,
+          mimetype='application/json'
+  )
+  return response
+
+@app.route('/metrics')
+def metrics():
+  response = app.response_class(
+          response=json.dumps({"db_connection_count": db_connection_count, "post_count": get_post_count()}),
+          status=200,
+          mimetype='application/json'
+  )
+  return response
+
 # start the application on port 3111
 if __name__ == "__main__":
+   logging.basicConfig(filename='app.log',level=logging.DEBUG)
    app.run(host='0.0.0.0', port='3111')
